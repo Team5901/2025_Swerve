033712ed4f0887d2a60680f73fbcc5c7457bc97e
jginnard <jginnard@users.noreply.github.com>
2025-03-24 23:35:10 -0400

    Calculate distance to target, target position from field layout.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,12 +6,25 @@ package frc.robot;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -25,9 +38,18 @@ public class Robot extends TimedRobot {
 
   private PhotonCamera camera;
 
+  private PhotonPoseEstimator photonPoseEstimator;
+
+  AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+
+  Optional<EstimatedRobotPose> robotPose;
+
   public Robot() {
     m_robotContainer = new RobotContainer();
     camera = new PhotonCamera("front_camera");
+    Transform3d robotToCam = new Transform3d(new Translation3d(0.5, 0.0, 0.5), new Rotation3d(0,0,0)); //Cam mounted facing forward, half a meter forward of center, half a meter up from center.
+    photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, robotToCam);
+    
   }
 
   @Override
@@ -71,10 +93,22 @@ public class Robot extends TimedRobot {
     // Read in relevant data from the Camera
     boolean targetVisible = false;
     double targetYaw = 0.0;
+    double targetPitch = 0.0;
     double targetRange = 0.0;
     int targetID = 0;
+    Optional<Pose3d> targetPose;
+    double targetHeight = 0.0;
+    double targetX = 0.0;
+    double targetY = 0.0;
     int resultCount = 0;
     int targetCount = 0;
+    Translation2d targetTranslation;
+
+    double poseTarget = 0;
+    double poseX = 0;
+    double poseY = 0;
+    double poseHeading = 0;
+
     List<PhotonPipelineResult> results = camera.getAllUnreadResults();
 
     resultCount = results.size();
@@ -85,33 +119,62 @@ public class Robot extends TimedRobot {
         targetCount = result.getTargets().size();
         if (result.hasTargets()) {
             // At least one AprilTag was seen by the camera
+
+            // Pose Estimation not working yet
+            robotPose = photonPoseEstimator.update(result);
+            if (robotPose.isPresent()) {
+                poseTarget = robotPose.get().targetsUsed.get(0).fiducialId;
+                poseX = robotPose.get().estimatedPose.getTranslation().getX();
+                poseY = robotPose.get().estimatedPose.getTranslation().getY();
+                poseHeading = Units.radiansToDegrees(robotPose.get().estimatedPose.getRotation().getAngle());
+            }
             for (PhotonTrackedTarget target : result.getTargets()) {
                 if (target.getFiducialId() > 0) {
                     targetID = target.getFiducialId();
+                    targetPose = aprilTagFieldLayout.getTagPose(targetID);
+                    if(targetPose.isPresent()) {
+                        targetHeight = targetPose.get().getTranslation().getZ();
+                        targetX = targetPose.get().getTranslation().getX();
+                        targetY = targetPose.get().getTranslation().getY();
+                    }
                     // Found Tag, record its information
-                    targetYaw = target.getYaw();
+                    targetYaw = Units.degreesToRadians(target.getYaw());
+                    targetPitch = Units.degreesToRadians(target.getPitch());
+                    // IMPORTANT: Won't work if target and camera are exact same height
                     targetRange =
                             PhotonUtils.calculateDistanceToTargetMeters(
                                     1, // Measured with a tape measure, or in CAD.
-                                    1, // From 2024 game manual for ID
-                                    Units.degreesToRadians(-30.0), // Measured with a protractor, or in CAD.
+                                    targetHeight, // From 2024 game manual for ID
+                                    Units.degreesToRadians(0), // Measured with a protractor, or in CAD.
                                     Units.degreesToRadians(target.getPitch()));
-
                     targetVisible = true;
+                    targetTranslation = PhotonUtils.estimateCameraToTargetTranslation(targetRange, Rotation2d.fromDegrees(-target.getYaw()));
                 }
             }
         }
     }
 
     // Put debug information to the dashboard
-    SmartDashboard.putBoolean("Camera Connected", camera.isConnected());
-    SmartDashboard.putNumber("Vision Result Count", (double) resultCount);
-    SmartDashboard.putNumber("Vision Target Count", (double) targetCount);
-    SmartDashboard.putBoolean("Vision Target Visible", targetVisible);
-    SmartDashboard.putNumber("Vision Target Range (m)", targetRange);
-    SmartDashboard.putNumber("Vision Target Yaw", targetYaw);
-    SmartDashboard.putNumber("Target ID", targetID);
     SmartDashboard.putNumber("Time", new Date().getTime());
+    SmartDashboard.putBoolean("Camera Connected", camera.isConnected());
+    SmartDashboard.putNumber("Result Count", (double) resultCount);
+    SmartDashboard.putNumber("Target Count", (double) targetCount);
+    SmartDashboard.putBoolean("Target Visible", targetVisible);
+    SmartDashboard.putNumber("Target ID", targetID);
+    SmartDashboard.putNumber("Target Range (m)", targetRange);
+    SmartDashboard.putNumber("Target Yaw", targetYaw);
+    SmartDashboard.putNumber("Target Pitch", targetPitch);
+
+    // Target Position
+    SmartDashboard.putNumber("Target Height", targetHeight);
+    SmartDashboard.putNumber("Target X", targetX);
+    SmartDashboard.putNumber("Target Y", targetY);
+
+    // Pose Estimation not working yet
+    SmartDashboard.putNumber("Target used for pose", poseTarget);
+    SmartDashboard.putNumber("Pose X", poseX);
+    SmartDashboard.putNumber("Pose Y", poseY);
+    SmartDashboard.putNumber("Pose Heading", poseHeading);
   }
 
   @Override
